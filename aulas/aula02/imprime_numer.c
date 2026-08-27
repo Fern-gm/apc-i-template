38222d868f23630ea6e4700b1b47cb920d9a112c
Pedro <ferndev68@gmail.com>
2026-08-26 21:53:44 -0300

    codigo da aula2

--- aulas/aula02/imprime_numer.c
+++ aulas/aula02/imprime_numer.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(){
-   printf("%f", 10.5);
+   printf("CPF: %lli\n",11111111111);
+   printf("CPF: %011i\n\n",00000000001);
+   printf("Preco = %7.2f\n", 1000.0);
+   printf("Preco = %7.2f\n", 100.0);
+   printf("Preco = %7.2f\n", 10.0);
+   printf("Preco = %7.2f\n", 10.0);
+   printf("Preco = %7.2f\n", 10.0);
+   printf("Preco = %7.2f\n", 10.0);
     return 0;
 }
--- aulas/aula02/imprime_texto.c
+++ aulas/aula02/imprime_texto.c
@@ -2,7 +2,18 @@
 int main(){
     //funcao par esrever algo na tela
     printf("C e muito legal\n");
-    printf("Vamos codar");
-    
+    printf("Vamos codar\n\n");
+    printf("A terceira letra do alfabeto %c e a ultima eh %c\n\n", 'C','Z');
+    printf("Ola, %s!\n\n", "Pedro");
+    printf("==============================================================\n");
+    printf("                     NOTA LEGAL\n");
+    printf("==============================================================\n");
+    printf("Item        Unid    Preco    Valor\n");
+    printf("Caneta      1       1,00     1,00\n");
+    printf("Borracha    1       5,00     5,00\n");
+    printf("Lapis       2       2,50     5,00\n");
+    printf("------------------------------------------------------------\n");
+    printf("TOTAL: R$ 11,00\n");
+    printf("------------------------------------------------------------");
     return 0;
-}
+} 
